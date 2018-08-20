909da7e32f43306505700924ae0075b4b94c2bc4
wuhxxx <wuhxxx@gmail.com>
2018-08-19 22:43:41 -0700

    Fix bugs and update readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # todo-list
+
+A simple but pretty todo list app, built with html and css, no backend implemented yet(may implement one in the future).
--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,4 +1,3 @@
-
 // check off specific todos by clicking (method 1, deprecated)
 // $("li").on("click", function() {
 // 	// check if li is currently grey(deleted)
@@ -17,55 +16,56 @@
 // 			textDecoration: "line-through"
 // 		});
 // 	}
-	
+
 // });
 
 // check off specific todos by clicking (method 2)
 // select ul but not li to support furture added li's functionality
 $("ul").on("click", "li", function() {
-	$(this).toggleClass("completed");
+    $(this).toggleClass("completed");
 });
 
 // delete todo item by clicking X
 // pass a event variable into it
 // select ul but not li to support furture added span's functionality
 $("ul").on("click", "span", function(e) {
-	// from "this" to select parent and fade out then remove
-	$(this).parent().fadeOut(500, function() {
-		$(this).remove();
-	});
+    // from "this" to select parent and fade out then remove
+    $(this)
+        .parent()
+        .fadeOut(500, function() {
+            $(this).remove();
+        });
 
-	// call jquery's stopPropagation() function to avoid triggerring li's click event listener
-	e.stopPropagation();
+    // call jquery's stopPropagation() function to avoid triggerring li's click event listener
+    e.stopPropagation();
 });
 
-
 $("input[type='text']").keypress(function(event) {
-	// check if the pressed key is enter.
-	// ascii code 13 for enter
-	if(event.which === 13) {
-		// grabbing the text from input
-		let todoText = $(this).val();
-		// clear current text input
-		$(this).val("");
-		// creat a new li text
-		let todoLi = `<li><span>X</span> ${todoText} </li>`;
-		// add new li text to ul
-		$("ul").append(todoLi);
-	}
+    // check if the pressed key is enter.
+    // ascii code 13 for enter
+    if (event.which === 13) {
+        // grabbing the text from input
+        let todoText = $(this).val();
+        // clear current text input
+        $(this).val("");
+        // creat a new li text
+        let todoLi = `<li><span><i class="fas fa-trash-alt"></i></span> ${todoText} </li>`;
+        // add new li text to ul
+        $("ul").append(todoLi);
+    }
 });
 
 var isPlus = true;
 
 $("h1 span").on("click", function() {
-	if (isPlus) {
-		// make minus icon
-		$(this).html(`<i class="fas fa-minus"></i>`);
-		isPlus = false;
-	} else {
-		// make plus icon
-		$(this).html(`<i class="fas fa-plus"></i>`);
-		isPlus = true;
-	}
-	$("input[type='text']").fadeToggle();
+    if (isPlus) {
+        // make minus icon
+        $(this).html(`<i class="fas fa-minus"></i>`);
+        isPlus = false;
+    } else {
+        // make plus icon
+        $(this).html(`<i class="fas fa-plus"></i>`);
+        isPlus = true;
+    }
+    $("input[type='text']").fadeToggle();
 });
